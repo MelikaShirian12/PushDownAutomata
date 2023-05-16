6258d92973d2242ea36c973076f775a784d4fa34
Zahra <zahraamasoumi@gmail.com>
2023-05-16 21:50:34 +0330

    Add second part of project

--- PDAproject/src/Main.java
+++ PDAproject/src/Main.java
@@ -1,5 +1,57 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        PushDownAutomaton pda = new PushDownAutomaton();
+
+        System.out.print("Enter the input string: ");
+        String input = sc.nextLine();
+        System.out.print("The string '" + input + "' is ");
+        if (pda.accept(input)) System.out.println("Accepted");
+        else System.out.println("Rejected");
+    }
+}
+
+class PushDownAutomaton {
+    public boolean accept(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        if (!input.startsWith("a")) return false;
+
+        stack.push('a');
+        int i = 1;
+        while (i < input.length()) {
+            char ch = input.charAt(i);
+
+            if (stack.isEmpty()) return false;
+
+            if (ch == 'a' && stack.peek() == 'a') {
+                i++;
+
+            } else if (ch == 'b') {
+
+                if (stack.peek() == 'a') {
+                    stack.pop();
+                    stack.push(ch);
+                    i++;
+                } else if (stack.peek() == 'b') {
+                    stack.push(ch);
+                    i++;
+                } else return false;
+
+            } else if (ch == 'c') {
+
+                if (stack.peek() != 'b') return false;
+
+                stack.pop();
+                i++;
+
+            } else return false;
+        }
+
+        return stack.isEmpty();
     }
 }
