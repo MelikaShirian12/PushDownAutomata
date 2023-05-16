3708da7865a2875ba44b3b89248baacaac1f45fc
melika shirian <106620989+emika12@users.noreply.github.com>
2023-05-16 20:56:03 +0430

    adding a and b generator

--- PDAproject/src/Main.java
+++ PDAproject/src/Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -14,22 +16,17 @@ public class Main {
                 case "1":
                     input = sc.nextLine();
                     while(!input.equals("Back"))
-                    {
                         input = sc.nextLine();
-                    }
+
                     break;
                 case "2":
                     input = sc.nextLine();
                     while(!input.equals("Back"))
-                    {
-
                         input = sc.nextLine();
-                    }
+
 
                     break;
                 default:
-
-
                     break;
             }
 
@@ -40,6 +37,33 @@ public class Main {
 
         }
 
+    }
 
+
+    public static String stringGenerator(int n , int m){
+        Stack <String> stack = new Stack<>();
+
+        String result = "";
+        stack.push("zero");
+
+        if (n > 0) {
+            stack.push("a");
+            for (int i = 0; i < n-1 ; ++i) {
+                result += stack.pop();
+                stack.push("a");
+            }
+        }
+        if (m >0 ){
+
+            result += stack.pop();
+            stack.push("b");
+
+            for (int i = 0; i < m-1 ; ++i) {
+                stack.push("b");
+                result += stack.peek();
+            }
+        }
+
+        return result;
     }
 }
