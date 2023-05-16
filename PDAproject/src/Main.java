9048e0a2987e871aebaaaddb7cc46c7249bc0ac1
Zahra <zahraamasoumi@gmail.com>
2023-05-16 22:56:40 +0330

    File editted: second part is added

--- PDAproject/src/Main.java
+++ PDAproject/src/Main.java
@@ -2,21 +2,109 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        String cmd = "default";
 
-        PushDownAutomaton pda = new PushDownAutomaton();
+        while (!cmd.equals("Exit")) {
+            System.out.print("Select the command\n1) String generator\n2) String Checker\n-> ");
 
-        System.out.print("Enter the input string: ");
-        String input = sc.nextLine();
-        System.out.print("The string '" + input + "' is ");
-        if (pda.accept(input)) System.out.println("Accepted");
-        else System.out.println("Rejected");
+            cmd = sc.nextLine();
+            String input;
+
+            switch (cmd) {
+                case "1" -> {
+                    input = "default";
+                    while (!input.equals("Back")) {
+                        System.out.print("\u001B[36m" + "Enter the m and n seperated with ',' ; or \"back\" to return to menu -> " + "\u001B[0m");
+                        input = sc.nextLine();
+
+                        String[] arguments = input.split(",");
+
+                        try {
+                            System.out.println("\u001B[35m" + "The result of this language is: " + "\u001B[0m" + "\u001B[36m" +
+                                    stringGenerator(Integer.parseInt(arguments[0]), Integer.parseInt(arguments[1])) + "\u001B[0m");
+
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                }
+
+                case "2" -> {
+                    input = "default";
+                    while (!input.equals("Back")) {
+                        System.out.print("\u001B[35m" + "Enter the string or \"back\" to return to menu -> " + "\u001B[0m");
+                        input = sc.nextLine();
+
+                        try {
+                            if (PushDownAutomaton.accept(input))
+                                System.out.println("\u001B[35m" + "The result is: " + "\u001B[0m" + "\u001B[36m" +
+                                        "The automata accepts this string" + "\u001B[0m");
+                            else
+                                System.out.println("\u001B[35m" + "The result is: " + "\u001B[0m" + "\u001B[36m" +
+                                        "The automata rejects this string" + "\u001B[0m");
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                }
+            }
+
+            System.out.print("\nSelect the command\n1) String generator\n2) String Checker\n-> ");
+            cmd = sc.nextLine();
+
+        }
+
+    }
+
+    public static String stringGenerator(int n, int m) throws Exception {
+        Stack<String> stack = new Stack<>();
+
+        StringBuilder result = new StringBuilder();
+        stack.push("$");
+
+        if (n > 0) {
+            stack.push("a");
+            for (int i = 0; i < n - 1; ++i) {
+                result.append(stack.pop());
+                stack.push("a");
+            }
+        }
+        if (m > 0) {
+
+            result.append(stack.pop());
+            stack.push("b");
+
+            for (int i = 0; i < m - 1; ++i) {
+                stack.push("b");
+                result.append(stack.peek());
+            }
+
+            result.append(stack.pop());
+            result.append("c");
+
+            for (int i = 0; i < m - 1; ++i) {
+                stack.pop();
+                result.append("c");
+            }
+
+
+        } else
+            result.append(stack.pop());
+
+
+        if (!stack.pop().equals("$"))
+            throw new Exception("Something went wrong !");
+
+        return result.toString();
     }
 }
 
 class PushDownAutomaton {
-    public boolean accept(String input) {
+    public static boolean accept(String input) {
         Stack<Character> stack = new Stack<>();
 
         if (!input.startsWith("a")) return false;
@@ -55,3 +143,32 @@ class PushDownAutomaton {
         return stack.isEmpty();
     }
 }
+
+/*
+                                                    a , a ➝ a                b , λ ➝ b              c , b ➝ λ
+                                                      ┌---┐                    ┌---┐                   ┌---┐
+                                                      |   |                    |   |                   |   |
+                                                      |   ⬇                    |   ⬇                   |   ⬇
+    ┌────┐     λ , λ ➝ λ    ┌────┐     a , λ ➝ a    ┌────┐    b , a ➝ b     ┌────┐    c , b ➝ λ     ┌────┐
+    │ q0 │-----------------➝│ q1 │-----------------➝│ q2 │-----------------➝│ q3 │-----------------➝│ q4 │
+    └────┘                   └────┘                  └────┘                   └────┘                  └────┘
+                                │                      │                        ⬆                         │
+                                │                      │                        │                         │
+                                └----------------------│------------------------┘                         │
+                                                       │       b, λ ➝ b                                   │
+                                                       │                                                  │
+                                                       │                      ┌────┐                      │
+                                                       └---------------------⟶│ q5 │⟵---------------------┘
+                                                              a , a ➝ λ      └────┘      λ , $ ➝ $
+                                                                            Final state
+
+     -- UNIVERSITY OF ISFAHAN --
+LANGUAGES AND AUTOMATA THEORY COURSE
+
+              AUTHORS:
+          MELIKA  SHIRIAN
+          ZAHRA   MASOUMI
+          KIANOOSH VADAEI
+
+
+ */
