88e256e4b38c5f01140b47df525246d730d584f1
Kianoosh <kia.comp.eng_outlook.com>
2023-05-16 17:56:19 +0430

    main ui

--- PDAproject/src/Main.java
+++ PDAproject/src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Select the command\n1) String generator\n2) String Checker\n-> ");
+        String cmd = sc.nextLine();
+        while(!cmd.equals("Exit"))
+        {
+            String input;
+            switch (cmd)
+            {
+                case "1":
+                    input = sc.nextLine();
+                    while(!input.equals("Back"))
+                    {
+                        input = sc.nextLine();
+                    }
+                    break;
+                case "2":
+                    input = sc.nextLine();
+                    while(!input.equals("Back"))
+                    {
+
+                        input = sc.nextLine();
+                    }
+
+                    break;
+                default:
+
+
+                    break;
+            }
+
+
+            System.out.printf("\nSelect the command\n1) String generator\n2) String Checker\n-> ");
+            cmd = sc.nextLine();
+
+
+        }
+
+
     }
 }
